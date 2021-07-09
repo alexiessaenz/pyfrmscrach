200a0d8d202cf7974faf68b2488ee3ac5e1da8a2
feeewq3 <00400815@uca.edu.sv>
2021-07-08 21:57:00 -0600

    hola first commit

--- aritmetico.py
+++ aritmetico.py
@@ -1,7 +1,6 @@
 print("ingrese el primer valor:")
 num1 = int (input())
-print3
-("ingrese el segundo valor:")
+print("ingrese el segundo valor:")
 num2 = int(input())
 resultado = num1 + num2
 print(resultado)
